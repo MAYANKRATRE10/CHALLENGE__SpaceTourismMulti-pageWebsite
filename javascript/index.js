211bb68b5f448eeb4fee89bfc86c725f6cc654dc
Mayank Kumar Ratre <82997237+MAYANKRATRE10@users.noreply.github.com>
2022-03-22 11:16:11 +0530

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+function open_menu_bar(){
+    document.getElementById("nav_bar").style.display="inline-block";
+}
+function close_menu_bar(){
+    document.getElementById("nav_bar").style.display="none";
+}
