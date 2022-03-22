e34eecbab081c790ca93b6177ecb35d047408501
Mayank Kumar Ratre <82997237+MAYANKRATRE10@users.noreply.github.com>
2022-03-22 11:15:27 +0530

    Delete assets directory

--- assets/javascript/index.js
+++
@@ -1,6 +0,0 @@
-function open_menu_bar(){
-    document.getElementById("nav_bar").style.display="inline-block";
-}
-function close_menu_bar(){
-    document.getElementById("nav_bar").style.display="none";
-}
